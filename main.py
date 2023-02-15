0e5e1d05bff7699d9c0068a886d9437abcb35523
Jaloliddin <jaloliddinmurodullayev337@gmail.com>
2023-02-15 17:32:08 +0500

    added methods

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 
 echo_app = Flask(__name__)
 
-@echo_app.route('/')
+@echo_app.route('/', methods=['GET', 'POST'])
 def echo():
-    return 'OK'
+    if request.method == 'GET':
+        return 'Hi there!'
+    elif request.method == 'POST':
+        data = request.get_json(force=True)
+        print(data)
+        return 'Hello'
 
 if __name__ == '__main__':
     echo_app.run()
--- set_webhook.py
+++ set_webhook.py
@@ -4,4 +4,5 @@ import os
 TOKEN = os.environ['TOKEN']
 bot = Bot(token=TOKEN)
 
+bot.set_webhook('https://vertow.pythonanywhere.com/')
 print(bot.get_webhook_info())
